561195cf92d31738777d44071a5a24e4ec9653b9
shlee <navystyle@naver.com>
2018-07-14 15:53:22 +0900

    .

--- tmp/cache_router/controllers.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Slim annotations router controllers cache file, self generated on 2018-07-08T15:34:29+09:00
- */
-
-$this->_usedControllers = ['/home/vagrant/work/tab/src/App/Controllers/UserController.php'];
--- tmp/cache_router/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Slim annotations router routes cache file, self generated on 2018-07-08T15:34:29+09:00
- */
-
-$this->map(['GET'], '/users', function($request, $response, $args) { return $this->triggerControllerAction('App\Controllers\UserController', 'indexAction', $args); })->setName('users');
-$this->map(['GET'], '/users/{id:[0-9]+}', function($request, $response, $args) { return $this->triggerControllerAction('App\Controllers\UserController', 'showAction', $args); })->setName('users.show');
-$this->map(['POST'], '/users', function($request, $response, $args) { return $this->triggerControllerAction('App\Controllers\UserController', 'postAction', $args); })->setName('users.post');
